764d2a6dfa4dc0b4be695b91672906f169bf07ef
srinivas <srinivaschowdary330@gmail.com>
2026-07-29 16:29:09 +0530

    Implement controller architecture

---
+++ backend/app/controllers/home_controller.py
@@ -0,0 +1,6 @@
+def home():
+    return {
+        "project": "Context-Aware Personalized News Recommendation System",
+        "status": "Backend Running Successfully 🚀",
+        "version": "0.4.0"
+    }
--- backend/app/routes/home_routes.py
+++ backend/app/routes/home_routes.py
@@ -1,11 +1,8 @@
 from flask import Blueprint
+from app.controllers.home_controller import home
 
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
-def home():
-    return {
-        "project": "Context-Aware Personalized News Recommendation System",
-        "status": "Backend Running Successfully 🚀",
-        "version": "0.3.0"
-    }
+def home_route():
+    return home()
